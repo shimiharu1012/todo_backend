1510ef933ecf79ec734b9bec24770f864564bc68
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2024-06-08 20:08:08 +0900

    [add] DBの接続・スキーマの作成，ルーティングの記述

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express= require("express");
+const mongoose=require('mongoose');
+const todo=require('./model/todo');
+const router=require('./routes/router')
+
 const app=express();
-const portNum=3001
-
-
-app.listen(portNum,console.log("サーバーが起動しました"))
+const portNum=3001;
+app.use(router)
 app.use(express.json())
 
+app.listen(portNum,console.log("サーバーが起動しました"))
+mongoose.connect('mongodb+srv://shimizuharuki314:tutorial@cluster0.lhsxstu.mongodb.net/')
+.then(()=>console.log('DB connection is OK!!'))
+.catch((err)=>console.log(err))
 
 
 app.get("/",(req,res)=>{
